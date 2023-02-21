9540eba470db96cf6e437a1041e8df8ee4685071
Philipp Khromov <filooloog@mail.ru>
2023-02-21 17:58:43 +0300

    feat: implement Header

---
+++ src/layouts/Header/Header.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+import Wrapper from "@components/Wrapper";
+import BurgerBackground from "@layouts/Header/BurgerBackground";
+import BurgerMenu from "@layouts/Header/BurgerMenu";
+import HeaderLogo from "@layouts/Header/HeaderLogo";
+import Navigation from "@layouts/Header/Navigation";
+
+import classes from "./Header.module.scss";
+
+export const Header = (): JSX.Element => {
+  const [isBurgerActive, setIsBurgerActive] = useState(false);
+
+  return (
+    <header className={classes.header}>
+      <Wrapper centered>
+        <div className={classes.header__inner}>
+          <HeaderLogo />
+          <Navigation isBurgerActive={isBurgerActive} />
+          <div className={classes.header__burgerContainer}>
+            <BurgerMenu
+              isBurgerActive={isBurgerActive}
+              setIsBurgerActive={setIsBurgerActive}
+            />
+          </div>
+          <BurgerBackground
+            isBurgerActive={isBurgerActive}
+            setIsBurgerActive={setIsBurgerActive}
+          />
+        </div>
+      </Wrapper>
+    </header>
+  );
+};
