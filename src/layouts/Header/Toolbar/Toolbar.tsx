4fe2fdf6b2e549b122852adf968d47893b835e8e
Philipp Khromov <filooloog@mail.ru>
2023-02-21 17:29:30 +0300

    feat: implement Header Toolbar

---
+++ src/layouts/Header/Toolbar/Toolbar.tsx
@@ -0,0 +1,23 @@
+import { ReactComponent as CartIcon } from "@assets/svg/cart.svg";
+import { ReactComponent as ProfileIcon } from "@assets/svg/user.svg";
+
+import classes from "./Toolbar.module.scss";
+
+export const Toolbar = (): JSX.Element => {
+  return (
+    <div className={classes.toolbar}>
+      <ul className={classes.toolbar__list}>
+        <li className={classes.toolbar__item}>
+          <a href="#">
+            <CartIcon className={classes.toolbar__icon} />
+          </a>
+        </li>
+        <li className={classes.toolbar__item}>
+          <a href="#">
+            <ProfileIcon className={classes.toolbar__icon} />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
