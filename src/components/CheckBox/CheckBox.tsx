ce01cb4675065711bf43e085ca3a67688774846a
Philipp Khromov <filooloog@mail.ru>
2023-02-21 19:27:25 +0300

    feat: add CheckBox

---
+++ src/components/CheckBox/CheckBox.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import classes from "./CheckBox.module.scss";
+
+export type CheckBoxProps = Omit<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  "onChange"
+> & {
+  /** Вызывается при клике на чекбокс */
+  onChange: (value: boolean) => void;
+};
+
+export const CheckBox = ({
+  onChange,
+  checked,
+  ...nativeProps
+}: CheckBoxProps): JSX.Element => {
+  return (
+    <label className={classes.checkbox}>
+      <input
+        type="checkbox"
+        className={classes.checkbox__input}
+        checked={checked}
+        onChange={(e) => onChange(e.target.checked)}
+        {...nativeProps}
+      />
+      <div className={classes.checkbox__checkmark} />
+    </label>
+  );
+};
