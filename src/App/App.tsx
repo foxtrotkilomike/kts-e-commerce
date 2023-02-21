35b630b8dada44e4abe65ca6477d91b090fd1f8e
Philipp Khromov <filooloog@mail.ru>
2023-02-21 18:01:01 +0300

    feat: add Header to App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,14 @@
-import classes from './App.module.scss';
+import "@styles/main.scss";
+import Header from "@layouts/Header";
+
+import classes from "./App.module.scss";
 
 export const App = (props: AppProps): JSX.Element => {
   return (
-    <div></div>
-  )
+    <div className={classes.app}>
+      <Header />
+    </div>
+  );
 };
 
 type AppProps = Record<string, string>;
