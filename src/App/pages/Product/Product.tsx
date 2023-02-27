81ff1918875b1ccaa91e46818db0cf5e909f1c15
Philipp Khromov <filooloog@mail.ru>
2023-02-27 14:18:06 +0300

    feat: add useFetchProduct for single Product fetching

--- src/App/pages/Product/Product.tsx
+++ src/App/pages/Product/Product.tsx
@@ -1,11 +1,10 @@
 import { useMemo } from "react";
 
 import Wrapper from "@components/Wrapper";
-import { DEFAULT_ERROR_STATUS, DEFAULT_PRODUCT_ID } from "@config/constants";
+import { DEFAULT_PRODUCT_ID } from "@config/constants";
 import { productsMock } from "@config/data";
-import ApiError from "@customTypes/ApiError";
-import ProductType from "@customTypes/Product";
-import useFetchProducts, { FetchFunctionParams } from "@hooks/useFetchProducts";
+import GetProductByIdConfig from "@customTypes/GetProductByIdConfig";
+import useFetchProduct from "@hooks/useFetchProduct";
 import SelectedProduct from "@pages/Product/components/SelectedProduct";
 import { getProductById } from "@services/products";
 import { useParams } from "react-router-dom";
@@ -16,18 +15,18 @@ const Product = (): JSX.Element => {
   const { productId } = useParams();
   const productIdNumber = productId ? Number(productId) : DEFAULT_PRODUCT_ID;
 
-  const fetchFunctionParams = useMemo(
-    () => [productIdNumber] as FetchFunctionParams,
-    [productIdNumber]
-  );
-  const [product, responseError] = useFetchProducts<ProductType>(
-    productsMock,
-    () => getProductById(productIdNumber),
-    fetchFunctionParams
-  ) as [ProductType, ApiError];
+  const fetchConfig = useMemo(() => {
+    return { productId: productIdNumber };
+  }, [productIdNumber]);
+
+  const { product, isLoading, responseError } =
+    useFetchProduct<GetProductByIdConfig>(
+      productsMock,
+      fetchConfig,
+      getProductById
+    );
 
   const isEmptyProduct = product.id === DEFAULT_PRODUCT_ID;
-  const isLoading = responseError.code === DEFAULT_ERROR_STATUS;
 
   return (
     <Wrapper main>
