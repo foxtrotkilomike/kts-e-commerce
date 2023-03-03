fd9106d906d51b6d568b0def26832e70150b98e7
Philipp Khromov <filooloog@mail.ru>
2023-03-03 22:52:52 +0300

    fix: add useEffect to useQueryParamsStoreInit

--- src/App/pages/Products/Products.tsx
+++ src/App/pages/Products/Products.tsx
@@ -1,11 +1,11 @@
 import Wrapper from "@components/Wrapper";
+import ProductStoreContextProvider from "@context/ProductStoreContextProvider";
 import Pages from "@customTypes/Pages";
 import { observer } from "mobx-react-lite";
 
 import Hero from "./components/Hero";
 import ProductsList from "./components/ProductsList";
 import Search from "./components/Search";
-import ProductStoreContextProvider from "../../../context/ProductStoreContextProvider";
 
 const Products = (): JSX.Element => {
   return (
--- src/hooks/useQueryParamsStoreInit.ts
+++ src/hooks/useQueryParamsStoreInit.ts
@@ -1,7 +1,11 @@
+import { useEffect } from "react";
+
 import rootStore from "@store/RootStore";
 import { useLocation } from "react-router-dom";
 
 export const useQueryParamsStoreInit = () => {
   const { search } = useLocation();
-  rootStore.query.setSearchQuery(search);
+  useEffect(() => {
+    rootStore.query.setSearchQuery(search);
+  });
 };
